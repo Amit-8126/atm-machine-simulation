c45b6ee1f90883ef4c137854b29b8b8a1d1eba93
Amit Negi <ikarannegi4919@gmail.com>
2025-12-27 21:05:24 +0530

    Add files via upload

---
+++ Accounts.py
@@ -0,0 +1,12 @@
+accounts = {
+    "1001": {"balance": 15000,
+             "history":[]
+        },
+    "1002": {"balance": 8000,
+             "history":[]
+        },
+    "1003": {"balance": 12000,
+             "history":[]
+            
+        }
+}
